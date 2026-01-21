be818912767d42b736470783f98a0d033b163b43
usagi-z <wzajaczkowski91@gmail.com>
2026-01-21 11:29:15 +0100

    add nix files

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Empty Template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [ ];
+        buildInputs = with pkgs; [ ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit nativeBuildInputs buildInputs;
+          packages = with pkgs; [ superhtml vscode-langservers-extracted ];
+        };
+      }
+    );
+}
